7c4da3a741614cba5427c26df50f2b9383c582d3
Sandy Woods <sandywoods@Sandys-Air-2.attlocal.net>
2022-08-29 04:30:58 -0400

    springboot-backend

---
+++ src/main/java/net/javaguides/springboot/SpringbootBackendApplication.java
@@ -0,0 +1,29 @@
+package net.javaguides.springboot;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import net.javaguides.springboot.model.User;
+import net.javaguides.springboot.repository.UserRepository;
+
+@SpringBootApplication
+public class SpringbootBackendApplication implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootBackendApplication.class, args);
+	}
+
+	@Autowired
+	private UserRepository userRepository;
+	
+	@Override
+	public void run(String... args) throws Exception {
+		this.userRepository.save(new User("Ramesh", "Fadartare", "ramesh@gmail.com"));
+		this.userRepository.save(new User("Tom", "Cruise", "tomsh@gmail.com"));
+		this.userRepository.save(new User("Tony", "Stark", "tony@gmail.com"));
+		
+	}
+
+}
